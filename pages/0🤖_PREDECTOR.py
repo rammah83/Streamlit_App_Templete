86542dc7070f26eebaa8f813b359351dc05224e5
rammah83 <rahimi.m.hicham@gmail.com>
2024-06-14 21:19:31 +0100

    Fix: fix pydantic data to feed model predictor

--- pages/0🤖_PREDECTOR.py
+++ pages/0🤖_PREDECTOR.py
@@ -1,33 +1,40 @@
+import numpy as np
 import streamlit as st
 
 
 from utils.config import set_shared_config
 from src.model import load_model, predict
-from src.helper import features
+from src.helper import features, DataToPredict
 
 set_shared_config()
 
-data = [None] * len(features)
+# General Layout
 left_col, middle_col, right_col = st.columns([1.5, 1, 1], gap="large")
 
-# Inputs
+# Disply Inputs
+data = dict.fromkeys(DataToPredict.__fields__.keys())
+
+
 for i, feat in enumerate(features):
     if isinstance(feat[-1], float):
-        data[i] = left_col.slider(*feat, )
+        data[feat[0]] = left_col.slider(*feat, )
     else:
-        middle_col.selectbox(*feat, help="Feature Discription")
+        data[feat[0]] = middle_col.selectbox(*feat, help="Feature Discription")
 
-# Prediction
+
+# Display Prediction
 with right_col:
     st.markdown(f"## Prediction")
     model = load_model()
+    X = list(DataToPredict(**data).dict().values())
+
     try:
-        X = data[:model.n_features_in_]
+        # X = data[:model.n_features_in_]
         result = model.predict([X])
         st.metric("# Prediction", value=result.round(2))
     except ValueError as e:
         # st.exception(e)
-        st.metric("# Prediction", value=-999.99)
+        st.metric("# Prediction", value='ERROR')
 
 
 with st.sidebar:
--- src/helper.py
+++ src/helper.py
@@ -1,26 +1,31 @@
 from pydantic import BaseModel, ValidationError, conlist, conint, constr
 
 features = [
-    ("Feature A", 0.0, 1.0, 0.1),
-    ("Feature B", 0.0, 1.0, 0.1),
-    ("Feature C", 0.0, 1.0, 0.1),
-    ("Feature D", 0.0, 1.0, 0.1),
-    ("Feature E", 0.0, 1.0, 0.1),
-    ("Feature X", ["Triangle", "Cercle", "Squarre"]),
-    ("Feature Y", ["Apple", "Orange", "Bannane"]),
-    ("Feature Z", ["Young", "Old"]),
+    ("Feature_A", 0.0, 1.0, 0.1),
+    ("Feature_B", 0.0, 1.0, 0.1),
+    ("Feature_C", 0.0, 1.0, 0.1),
+    ("Feature_D", 0.0, 1.0, 0.1),
+    ("Feature_E", 0.0, 1.0, 0.1),
+    # ("Feature_X", ["Triangle", "Cercle", "Squarre"]),
+    # ("Feature_Y", ["Apple", "Orange", "Bannane"]),
+    # ("Feature_Z", ["Young", "Old"]),
 ]
 
 
-class Data(BaseModel):
-    Feature_A: conlist(float, min_items=1)
-    Feature_B: conlist(float, min_items=1)
-    Feature_C: conlist(float, min_items=1)
-    Feature_D: conlist(float, min_items=1)
-    Feature_E: conlist(float, min_items=1)
-    Feature_X: conlist(str, min_items=1)
-    Feature_Y: conlist(str, min_items=1)
-    Feature_Z: conlist(str, min_items=1)
+class DataToPredict(BaseModel):
+    Feature_A: float
+    Feature_B: float
+    Feature_C: float
+    Feature_D: float
+    Feature_E: float
+    # Feature_A: conlist(float, min_items=1)
+    # Feature_B: conlist(float, min_items=1)
+    # Feature_C: conlist(float, min_items=1)
+    # Feature_D: conlist(float, min_items=1)
+    # Feature_E: conlist(float, min_items=1)
+    # Feature_X: conlist(str, min_items=1)
+    # Feature_Y: conlist(str, min_items=1)
+    # Feature_Z: conlist(str, min_items=1)
 
 if __name__ == "__main__":
     for feat in features:
